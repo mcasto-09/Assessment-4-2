08b1bfc2c247b04484190cef6ecaf7144ea84694
Mary Castorani <mgcastorani@gmail.com>
2024-06-24 13:15:01 -0600

    almost done with header

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -6,11 +6,10 @@ function App() {
 
   return (
     <div className="Header">
+      <h1>Home</h1>
       <h2 className="forYou">For You</h2>
       <h2 className="following">Following</h2>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   );
 }
