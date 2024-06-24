f234c178db4282da52888d42e8ac42b13126590c
Mary Castorani <mgcastorani@gmail.com>
2024-06-23 23:19:42 -0600

    header WIP

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,5 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
+function App() {
+
+  return (
+    <div className="Header">
+      <h2 className="forYou">For You</h2>
+      <h2 className="following">Following</h2>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
 
 export default App;
