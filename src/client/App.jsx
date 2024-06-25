5f32ab7fc20603a089ffca3efc753b4b28402cbe
Mary Castorani <mgcastorani@gmail.com>
2024-06-25 01:06:11 -0600

    wehh

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
+  
 
   return (
-    <div className="Header">
-      <h1>Home</h1>
-      <h2 className="forYou">For You</h2>
-      <h2 className="following">Following</h2>
-      
+    <div>
+      <div className="header">
+        <h1>Home</h1>
+        <h2 className="forYou">For You</h2>
+        <h2 className="following">Following</h2>
+      </div>
+      <div className="post">
+        <p className="text">look at this dude i found</p>
+        <img className="catStare" src="https://i.kym-cdn.com/entries/icons/facebook/000/043/403/cover3.jpg"></img>
+      </div>
     </div>
   );
 }
