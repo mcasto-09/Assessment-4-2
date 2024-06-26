a566b5732c72e6a3029d6f4dd51685d77e410e47
mcasto-09 <mgcastorani@gmail.com>
2024-06-25 19:32:03 -0600

    almost done

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,7 +3,16 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  
+  let [count, setCounter] = useState(0);
+  const click = () => {
+    setCounter(count + 1);
+  };
+  const repost = () => {
+    return alert ('Reposted')
+  }
+// posts array
+
+
 
   return (
     <div>
@@ -15,6 +24,12 @@ function App() {
       <div className="post">
         <p className="text">look at this dude i found</p>
         <img className="catStare" src="https://i.kym-cdn.com/entries/icons/facebook/000/043/403/cover3.jpg"></img>
+        <div className="responses">
+          <button className="like" onClick={click}>Likes:{count}</button>
+
+          <button className="repost" onClick={repost}>Repost</button>
+          <button className="comment">Comment</button>
+        </div>
       </div>
     </div>
   );
